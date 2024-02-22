e671026ea0914d8621d953c4855e7c94727eb8b3
Mohammed Muzzammil <36696171+mohammed-muzzammil@users.noreply.github.com>
2024-02-22 21:29:19 +0530

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -16,4 +16,4 @@ def catch_all(path):
     return jsonify({"message": "Request received and processed."})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',  port=443)
